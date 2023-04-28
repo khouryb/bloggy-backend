1bac2f3e1e58a7019c253f7d468e4e7c36f73c9f
Benjamin Khoury <khourybenjamin@gmail.com>
2023-04-28 15:05:04 +0100

    add create article route

--- routes/articles.js
+++ routes/articles.js
@@ -54,5 +54,19 @@ router.get('/api/articles', (req, res) => {
  *  Description:   Create a new article
  */
 
+router.post('/api/articles', (req, res) => {
+    Article.create(req.body.article)
+    // On a successful 'create' action, respond with 201
+    // HTTP status and the content of the new Article 
+    .then((newArticle) => {
+        res.status(201).json({ article: newArticle })
+    })
+    // Catch any errors that may occur
+    .catch((error) => {
+        res.status(500).json({ error: error })
+    })
+})
+
+
 //export the Router so we can use it in the 'server.js file
 module.exports = router
